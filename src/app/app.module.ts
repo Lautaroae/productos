a3b4070c6934ac91c20131146d252e64e5e13e30
Lautaroae <lautaroaimonetti61@gmail.com>
2022-09-07 12:21:45 -0300

    "commit 2"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 //Componentes
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
@@ -17,7 +19,9 @@ import { ListarProductosComponent } from './components/listar-productos/listar-p
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import{FormGroup,FormBuilder,Validators} from "@angular/forms"
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from 'src/app/model/producto';
 
 @Component({
   selector: 'app-crear-producto',
@@ -9,7 +12,7 @@ import{FormGroup,FormBuilder,Validators} from "@angular/forms"
 export class CrearProductoComponent implements OnInit {
   productoForm:FormGroup
 
-  constructor(private fb:FormBuilder) {
+  constructor(private fb:FormBuilder, private router:Router,private toastr: ToastrService) {
     this.productoForm=this.fb.group({
       producto:["",Validators.required],
       categoria:["",Validators.required],
@@ -22,6 +25,17 @@ export class CrearProductoComponent implements OnInit {
   }
   agregarProducto(){
     console.log(this.productoForm);
+    console.log(this.productoForm.get("producto")?.value);
+
+    const PRODUCTO:Producto={
+      nombre:this.productoForm.get("producto")?.value,
+      categoria:this.productoForm.get("categoria")?.value,
+      ubicacion:this.productoForm.get("ubicacion")?.value,
+      precio:this.productoForm.get("precio")?.value
+    }
+    console.log(PRODUCTO);
+    this.toastr.success('El producto fue registrado con exito!', 'Producto registrado');
+    this.router.navigate(["/"])
   }
 
 }
